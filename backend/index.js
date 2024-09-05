d83e1362b631e76ccc8829a02a7e1ea684601243
Remy2404 <meemii191@gmail.com>
2024-09-06 01:19:50 +0700

    updated help me pg

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const __dirname = path.dirname(__filename);
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL ||"http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     credentials: true,
   })
 );
