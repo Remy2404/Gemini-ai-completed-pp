86c10d5fa884315335d6da4e2f3f4c81ac9a8b1a
Remy2404 <meemii191@gmail.com>
2024-12-13 23:05:04 +0700

    fix: update static file paths for production and add build script

--- server/index.js
+++ server/index.js
@@ -164,10 +164,10 @@ app.use((err, req, res, next) => {
 });
 
 // PRODUCTION
-app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.static(path.join(__dirname, "./dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+  res.sendFile(path.join(__dirname, "../dist", "index.html"));
 });
 
 app.listen(port, () => {
