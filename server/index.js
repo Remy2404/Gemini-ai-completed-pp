62404e13e9ea572b3085bde19db18ae14e5c172d
Remy2404 <meemii191@gmail.com>
2024-12-13 23:26:24 +0700

    fix: update static file paths and improve build script for distribution

--- server/index.js
+++ server/index.js
@@ -167,7 +167,7 @@ app.use((err, req, res, next) => {
 app.use(express.static(path.join(__dirname, "./dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../dist", "index.html"));
+  res.sendFile(path.join(__dirname, "./dist", "index.html"));
 });
 
 app.listen(port, () => {
