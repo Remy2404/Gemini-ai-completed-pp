4830b9bc6f4f264d0c8a89a9bb2a2147873165d5
Remy2404 <meemii191@gmail.com>
2024-12-14 00:10:00 +0700

    fix: remove unused production static file serving in server index

--- server/index.js
+++ server/index.js
@@ -163,13 +163,6 @@ app.use((err, req, res, next) => {
   res.status(401).send("Unauthenticated!");
 });
 
-// PRODUCTION
-app.use(express.static(path.join(__dirname, "../dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../dist", "index.html"));
-});
-
 app.listen(port, () => {
   connect();
   console.log("Server running on 3000");
